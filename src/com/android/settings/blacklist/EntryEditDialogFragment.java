5e07fbfde4d5d3c1cd9767cb0541e80bac929b1c
Ankit Gupta <agupta@cyngn.com>
2016-05-16 17:08:45 -0700

    Settings: save parent fragment when orientation changes
    use setRetainInstance(boolean) to have fragment instance
    retained across configuration changes.
    
    Issue-id: FEIJ-645
    
    Change-Id: I013a7516a8998fde8281e8ba0be4c07d955ebebe

--- src/com/android/settings/blacklist/EntryEditDialogFragment.java
+++ src/com/android/settings/blacklist/EntryEditDialogFragment.java
@@ -296,6 +296,7 @@ public class EntryEditDialogFragment extends DialogFragment
         public static DialogFragment newInstance(EntryEditDialogFragment parent) {
             DialogFragment fragment = new DeleteConfirmationFragment();
             fragment.setTargetFragment(parent, 0);
+            fragment.setRetainInstance(true);
             return fragment;
         }
 
